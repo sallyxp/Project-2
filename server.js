11b346cdc632d7e835b015ba8cafa606f5fb54e7
George Cope <G.Cope13@gmail.com>
2021-03-31 18:54:27 +0100

    Initial commit to main branch. Outlined file structure and created basic server.js, config/connection.js files & db/schema.sql files

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const session = require('express-session');
+const path = require('path');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Enables use of helper functions
+const hbs = exphbs.create({helpers});
+
+const sess = {
+    secret: 'secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// app.use(express.static(path.join(__dirname, '<folder_name>')));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}. http://localhost:${PORT}`));
+});
