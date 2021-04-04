2c42a15677f36f31c618dc44873c12c479cdef6b
Olivia Owen <olivia.owen@hotmail.com>
2021-04-04 20:02:06 +0100

    Added function for add comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,1 +1,29 @@
-const 
+
+
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+    const comment_text = document.querySelector('#comment-body').value.trim();
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    if (comment_text) {
+        const response = await fetch(`/api/posts/${post_id}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id: post_id,
+                comment_text: comment_text,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+document
+    .querySelector('.comment-post-form')
+    .addEventListener('submit', commentFormHandler);
