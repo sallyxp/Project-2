d8e993abfe842aa9fc2f2c95add879a3cf10d8a5
Olivia Owen <olivia.owen@hotmail.com>
2021-04-04 20:46:15 +0100

    Added edit function for the comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -31,3 +31,32 @@ document
 
 
 // The below allows the user to edit a comment
+const editCommentHandler = async (event) => {
+    event.preventDefault();
+    const name = document.querySelector('input[name="edit-comment-title"]').value;
+    const description = document.querySelector('input[name="edit-comment-body"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+
+        const response = await fetch(`/dashboard/edit/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name,
+                description
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to update post');
+        }
+};
+
+document
+    .querySelector('.edit-comment-form')
+    .addEventListener('submit', editButtonHandler);
